bce473119f0f0c986d5b1ffa258bf1b87c30027f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-08 21:30:22 +0300

    Asp.NetCore8.0_RealEstate_Dapper_API+UI_Project Created

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
