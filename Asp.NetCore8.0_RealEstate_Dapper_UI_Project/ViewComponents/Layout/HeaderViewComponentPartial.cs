49bde0cb7a206e1609cd5f9051b2ac973c2013e0
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-15 22:58:02 +0300

    Components Added + Partial Views and Classes added + Home Page Design Prepared and Edited

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/Layout/HeaderViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.Layout
+{
+    public class _HeaderViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    }
