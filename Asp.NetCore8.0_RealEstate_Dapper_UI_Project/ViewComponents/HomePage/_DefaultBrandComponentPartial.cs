a98731592fe6db53c76edd8ee37503774df506c3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-17 19:50:11 +0300

    Appearance on the Home Page: Components Completed + Scripts Moved to Components + Some Errors Fixed on the Home Page + Some Arrangements Made to the Footer + New Classes Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/HomePage/_DefaultBrandComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.HomePage
+{
+    public class _DefaultBrandComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/HomePage/_DefaultScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.HomePage
+{
+    public class _DefaultScriptComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
