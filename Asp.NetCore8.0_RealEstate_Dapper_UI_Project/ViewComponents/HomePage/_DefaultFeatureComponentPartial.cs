e1e7ef2fdcb26bbe5127843efb9f1634d1a9d57a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-17 18:40:17 +0300

    Home Page Designs Edited + New View Components Added + Products +Popular Places Components Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/HomePage/_DefaultFeatureComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.HomePage
+{
+	public class _DefaultFeatureComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
