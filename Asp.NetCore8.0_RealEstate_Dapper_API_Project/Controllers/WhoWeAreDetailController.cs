5def689442a47f21a47f2871a5d710ac4466e67a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-18 19:24:23 +0300

    Who We Are Detail Controller CRUD Process Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/Controllers/WhoWeAreDetailController.cs
@@ -0,0 +1,51 @@
+﻿using Asp.NetCore8._0_RealEstate_Dapper_API_Project.DTOs.WhoWeAreDetailDTOs;
+using Asp.NetCore8._0_RealEstate_Dapper_API_Project.Models.Repositories.WhoWeAreRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhoWeAreDetailController : ControllerBase
+    {
+        private readonly IWhoWeAreDetailRepository _whoWeAreDetailRepository;
+
+        public WhoWeAreDetailController(IWhoWeAreDetailRepository whoWeAreDetailRepository)
+        {
+            _whoWeAreDetailRepository = whoWeAreDetailRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> WhoWeAreDetailList()
+        {
+            var values = await _whoWeAreDetailRepository.GetAllWhoWeAreDetailAsync();
+            return Ok(values);
+            //NULL UPDATE
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateWhoWeAreDetailAsync(CreateWhoWeAreDetailDTO createWhoWeAreDetailDTO)
+        {
+            _whoWeAreDetailRepository.CreateWhoWeAreDetail(createWhoWeAreDetailDTO);
+            return Ok("Hakkımızda Detayları Başarılı Bir Şekilde Eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteWhoWeAreDetail(int id)
+        {
+            _whoWeAreDetailRepository.DeleteWhoWeAreDetail(id);
+            return Ok("Hakkımızda Detayları Başarılı Bir Şekilde Silindi.");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDTO updateWhoWeAreDetailDTO)
+        {
+            _whoWeAreDetailRepository.UpdateWhoWeAreDetail(updateWhoWeAreDetailDTO);
+            return Ok("Hakkımızda Detayları Başarılı Bir Şekilde Güncellendi.");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetWhoWeAreDetail(int id)
+        {
+            var values = await _whoWeAreDetailRepository.GetWhoWeAreDetail(id);
+            return Ok(values);
+        }
+    }
+}
