04909369680da5f9c7e898159cd432a1548e3fc9
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-19 17:51:14 +0300

    Service Api Consume Process Added

--- Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -16,16 +16,20 @@ namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.HomePage
         public async Task< IViewComponentResult> InvokeAsync()
         {
             var client=_httpClientFactory.CreateClient();
+            var client2=_httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:44309/api/WhoWeAreDetail");
-            if(responseMessage.IsSuccessStatusCode)
+            var responseMessage2 = await client2.GetAsync("https://localhost:44309/api/Services");
+            if(responseMessage.IsSuccessStatusCode && responseMessage2.IsSuccessStatusCode)
             {
                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+               var jsonData2=await responseMessage2.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDTO>>(jsonData);
+                var values2 = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(jsonData2);
                 ViewBag.Title = values.Select(x=>x.Title).FirstOrDefault();
                 ViewBag.Subtitle = values.Select(x=>x.Subtitle).FirstOrDefault();
                 ViewBag.Description1 = values.Select(x=>x.Description1).FirstOrDefault();
                 ViewBag.Description2 = values.Select(x=>x.Description2).FirstOrDefault();
-                return View();
+                return View(values2);
             }
         return View();
         }
