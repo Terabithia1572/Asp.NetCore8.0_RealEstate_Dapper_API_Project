1448b27302a2ed7e0b8f14143a90e31267d320f3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-02 23:50:18 +0300

    Bottom Grids Controller Process Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/Controllers/BottomGridsController.cs
@@ -0,0 +1,24 @@
+﻿using Asp.NetCore8._0_RealEstate_Dapper_API_Project.Models.Repositories.BottomGridRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BottomGridsController : ControllerBase
+    {
+        private readonly IBottomGridRepository _bottomGridRepository;
+
+        public BottomGridsController(IBottomGridRepository bottomGridRepository)
+        {
+            _bottomGridRepository = bottomGridRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> BottomGridList()
+        {
+            var values = await _bottomGridRepository.GetAllBottomGridAsync();
+            return Ok(values);
+        }
+    }
+}
