0c836c53d3febe6f12d95808df43e93e3dc70ef2
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-03 22:31:36 +0300

    Admin + Category Controller Added + Header+ Sidebar + Spinner Components Added + Admin Layout Added + Category Page Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/AdminLayout/_AdminLayoutSpinnerComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutSpinnerComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
