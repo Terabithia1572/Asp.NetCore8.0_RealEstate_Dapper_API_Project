0620888905f3e6f861f9c7bdd5376dcae3a47b7e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-04 23:09:32 +0300

    Employee Controller Added + Employee Repository Process Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/Controllers/EmployeesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Asp.NetCore8.0_RealEstate_Dapper_API_Project/Models/Repositories/EmployeeRepositories/EmployeeRepository.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/Models/Repositories/EmployeeRepositories/EmployeeRepository.cs
@@ -1,32 +1,88 @@
-﻿using Asp.NetCore8._0_RealEstate_Dapper_API_Project.DTOs.EmployeeDTOs;
+﻿using Asp.NetCore8._0_RealEstate_Dapper_API_Project.DTOs.CategoryDTOs;
+using Asp.NetCore8._0_RealEstate_Dapper_API_Project.DTOs.EmployeeDTOs;
+using Asp.NetCore8._0_RealEstate_Dapper_API_Project.Models.DapperContext;
+using Dapper;
 
 namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.Models.Repositories.EmployeeRepositories
 {
     public class EmployeeRepository : IEmployeeRepository
     {
-        public void CreateEmployee(CreateEmployeeDTO createEmployeeDTO)
+        private readonly Context _context;
+
+        public EmployeeRepository(Context context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public void DeleteEmployee(int id)
+        public async void CreateEmployee(CreateEmployeeDTO createEmployeeDTO)
         {
-            throw new NotImplementedException();
+            //Bu kod, bir kategoriyi veritabanına eklemek için kullanılan bir metodu tanımlar. Metodun amacı, verilen kategoriyi veritabanına kaydetmektir. Aşağıda kodun detaylı açıklaması bulunmaktadır:
+            {
+
+                string query = "insert into Employee (EmployeeName,EmployeeTitle,EmployeeMail,EmployeePhoneNumber,EmployeeImageURL,EmployeeStatus) values (@employeeName,@employeeTitle,@employeeMail,@employeePhoneNumber,@employeeImageURL,@employeeStatus)";
+                var parameters = new DynamicParameters();
+                parameters.Add("@employeeName", createEmployeeDTO.EmployeeName);
+                parameters.Add("@employeeTitle", createEmployeeDTO.EmployeeTitle);
+                parameters.Add("@employeeMail", createEmployeeDTO.EmployeeMail);
+                parameters.Add("@employeePhoneNumber", createEmployeeDTO.EmployeePhoneNumber);
+                parameters.Add("employeeImageURL", createEmployeeDTO.EmployeeImageURL);
+                parameters.Add("@employeeStatus", true);
+                using (var connection = _context.CreateConnection())
+                {
+                    await connection.ExecuteAsync(query, parameters);
+                }
+            }
         }
 
-        public Task<List<ResultEmployeeDTO>> GetAllEmployeeAsync()
+        public async void DeleteEmployee(int id)
         {
-            throw new NotImplementedException();
+            string query = "delete from Employee where EmployeeID=@employeeID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@employeeID", id);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
         }
 
-        public Task<GetByIDEmployeeDTO> GetEmployee(int id)
+        public async Task<List<ResultEmployeeDTO>> GetAllEmployeeAsync()
         {
-            throw new NotImplementedException();
+            string query = "select * from Employee";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultEmployeeDTO>(query);
+                return values.ToList();
+            }
         }
 
-        public void UpdateEmployee(UpdateEmployeeDTO updateEmployeeDTO)
+        public async Task<GetByIDEmployeeDTO> GetEmployee(int id)
         {
-            throw new NotImplementedException();
+            string query = "Select * from Employee where EmployeeID=@employeeID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@employeeID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<GetByIDEmployeeDTO>(query, parameters);
+                return values;
+            }
+        }
+
+        public async void UpdateEmployee(UpdateEmployeeDTO updateEmployeeDTO)
+        {
+            string query = "update Employee Set EmployeeName=@employeeName,EmployeeTitle=@employeeTitle,EmployeeMail=@employeeMail,EmployeePhoneNumber=@employeePhoneNumber,EmployeeImageURL=@employeeImageURL,EmployeeStatus=@employeeStatus where EmployeeID=@employeeID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@employeeName", updateEmployeeDTO.EmployeeName);
+            parameters.Add("@employeeTitle", updateEmployeeDTO.EmployeeTitle);
+            parameters.Add("@employeeMail", updateEmployeeDTO.EmployeeMail);
+            parameters.Add("@employeePhoneNumber", updateEmployeeDTO.EmployeePhoneNumber);
+            parameters.Add("employeeImageURL", updateEmployeeDTO.EmployeeImageURL);
+            parameters.Add("@employeeStatus", true);
+            parameters.Add("@employeeID", updateEmployeeDTO.EmployeeID);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
         }
     }
 }
