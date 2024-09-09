4785cd51173f16b2d8911e07ba2382935390b5c9
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-10 01:02:37 +0300

    Popular Location CRUD Process + Pages added for UI

--- Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Controllers/PopularLocationController.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Controllers/PopularLocationController.cs
@@ -1,11 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.PopularLocationDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Controllers
 {
     public class PopularLocationController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PopularLocationController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44309/api/PopularLocations");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreatePopularLocation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePopularLocation(CreatePopularLocationDTO createPopularLocationDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createPopularLocationDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44309/api/PopularLocations", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeletePopularLocation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44309/api/PopularLocations/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdatePopularLocation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44309/api/PopularLocations/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdatePopularLocationDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdatePopularLocation(UpdatePopularLocationDTO updatePopularLocationDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updatePopularLocationDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44309/api/PopularLocations/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/DTOs/PopularLocationDTOs/CreatePopularLocationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.PopularLocationDTOs
+{
+    public class CreatePopularLocationDTO
+    {
+        public string PopularLocationCityName { get; set; }
+        public string PopularLocationImageURL { get; set; }
+    }
+}
