f6dffc4513bbd09efc70a8ef86a5c34476274b3c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-09 10:33:19 +0300

    Services Controller Process Added + Services DTO Process Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Controllers/ServiceController.cs
@@ -0,0 +1,84 @@
+﻿using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.ServiceDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44309/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateService(CreateServiceDTO createServiceDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createServiceDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44309/api/Service", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44309/api/Service/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44309/api/Service/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateServiceDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDTO updateServiceDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateServiceDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44309/api/Service/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/DTOs/ServiceDTOs/CreateServiceDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.ServiceDTOs
+{
+    public class CreateServiceDTO
+    {
+        public string ServiceName { get; set; }
+        public bool ServiceStatus { get; set; }
+    }
+}
