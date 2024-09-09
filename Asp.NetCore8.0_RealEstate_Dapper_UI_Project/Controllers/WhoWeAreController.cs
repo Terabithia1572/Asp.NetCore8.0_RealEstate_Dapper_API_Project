8645e2d2506fe559f87f7259a95eed608e8a2fa9
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-09 10:05:31 +0300

    Who We Are Controller + DTO Process Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Controllers/WhoWeAreController.cs
@@ -0,0 +1,84 @@
+﻿using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.WhoWeAreDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Controllers
+{
+    public class WhoWeAreController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public WhoWeAreController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44309/api/WhoWeAreDetail");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateWhoWeAreDetail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateWhoWeAreDetail(CreateWhoWeAreDetailDTO createWhoWeAreDetailDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createWhoWeAreDetailDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44309/api/WhoWeAreDetail", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteWhoWeAreDetail(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44309/api/WhoWeAreDetail/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateWhoWeAreDetail(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44309/api/WhoWeAreDetail/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateWhoWeAreDetailDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDTO updateWhoWeAreDetailDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateWhoWeAreDetailDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44309/api/WhoWeAreDetail/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/DTOs/WhoWeAreDTOs/CreateWhoWeAreDetailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.WhoWeAreDTOs
+{
+    public class CreateWhoWeAreDetailDTO
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description1 { get; set; }
+        public string Description2 { get; set; }
+    }
+}
