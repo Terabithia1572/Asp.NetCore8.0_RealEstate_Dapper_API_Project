10b1aba18dcd52805e11836d45b7af5a24d69a85
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-10 00:34:55 +0300

    Bottom Grid Process Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Controllers/BottomGridController.cs
@@ -0,0 +1,84 @@
+﻿using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.BottomGridDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Controllers
+{
+    public class BottomGridController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BottomGridController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44309/api/BottomGrids");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBottomGridDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateBottomGrid()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBottomGrid(CreateBottomGridDTO createBottomGridDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBottomGridDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44309/api/BottomGrids", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteBottomGrid(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44309/api/BottomGrids/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateBottomGrid(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44309/api/BottomGrids/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateBottomGridDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateBottomGrid(UpdateBottomGridDTO updateBottomGridDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateBottomGridDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44309/api/BottomGrids/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
