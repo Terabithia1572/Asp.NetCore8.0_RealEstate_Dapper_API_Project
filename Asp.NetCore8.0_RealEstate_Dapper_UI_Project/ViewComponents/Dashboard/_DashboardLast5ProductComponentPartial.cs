54406c01200b2cbf65c0ff03f28fd36ba4825edd
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-11 00:34:18 +0300

    Dashboard Process Contiune + Dashboard Last 5 Product Process Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/Dashboard/_DashboardLast5ProductComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.Dashboard
+{
+    public class _DashboardLast5ProductComponentPartial:ViewComponent
+    {
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
