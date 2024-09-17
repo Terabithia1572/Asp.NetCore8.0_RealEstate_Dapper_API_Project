2542d83af76404c619d24f26ef97e9fb9b3f160b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-17 23:41:09 +0300

    Signal R Updated

---
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/Hubs/ChatHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.Hubs
+{
+    public class ChatHub : Hub
+    {
+       
+    }
+}
--- Asp.NetCore8.0_RealEstate_Dapper_API_Project/Hubs/SignalRHub.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/Hubs/SignalRHub.cs
@@ -2,7 +2,20 @@
 
 namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.Hubs
 {
-    public class SignalRHub:Hub
+    public class SignalRHub : Hub
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public SignalRHub(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task SendCategoryCount()
+        {
+            var client1 = _httpClientFactory.CreateClient();
+            var responseMessage1 = await client1.GetAsync("https://localhost:44306/api/Statistics/CategoryCount");
+            var value1 = await responseMessage1.Content.ReadAsStringAsync();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value1);
+        }
     }
 }
