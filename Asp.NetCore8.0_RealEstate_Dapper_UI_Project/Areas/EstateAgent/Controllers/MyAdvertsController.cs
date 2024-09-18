cd11374f556017fdb467dd3dc1d43fcf92136c49
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-19 00:06:18 +0300

    List of advertisements published by the user Process Added

--- Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Areas/EstateAgent/Controllers/MyAdvertsController.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Areas/EstateAgent/Controllers/MyAdvertsController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.ProductDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Areas.EstateAgent.Controllers
 {
     [Area("EstateAgent")]
     public class MyAdvertsController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MyAdvertsController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            id = 3;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44309/api/Products/ProductAdvertsListByEmployee?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductAdvertListWithCategoryWithByEmployeeDTO>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
