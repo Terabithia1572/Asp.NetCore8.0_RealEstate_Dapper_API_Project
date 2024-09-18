918166b3db0ce1990e7ea295fbcbfefa860560d6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-18 23:30:36 +0300

    Estate Agent Panels Added + Area Added + New Estate Agent Pages Added

--- Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Program.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/Program.cs
@@ -24,5 +24,13 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 
 app.Run();
