d70e2791f9afc54214e2459f9601a7247b6a6a07
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-19 00:54:26 +0300

    Creating JWT in API for logged in user Process Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Asp.NetCore8._0_RealEstate_Dapper_API_Project.DTOs.LoginDTOs;
+using Asp.NetCore8._0_RealEstate_Dapper_API_Project.Models.DapperContext;
+using Asp.NetCore8._0_RealEstate_Dapper_API_Project.Tools;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(CreateLoginDTO createLoginDTO)
+        {
+            string query = "select * from AppUser where Username=@username and Password=@password";
+            string query2 = "select UserID from AppUser where Username=@username and Password=@password";
+            var parameters = new DynamicParameters();
+            parameters.Add("@username", createLoginDTO.Username);
+            parameters.Add("@password", createLoginDTO.Password);
+            using (var connection = _context.CreateConnection())
+            {
+                var values=await connection.QueryFirstOrDefaultAsync<CreateLoginDTO>(query,parameters);
+                var values2=await connection.QueryFirstOrDefaultAsync<GetAppUserIDDTO>(query2,parameters);
+                if(values != null)
+                {
+                    GetCheckAppUserViewModel getCheckAppUserViewModel = new GetCheckAppUserViewModel();
+
+                    getCheckAppUserViewModel.Username=createLoginDTO.Username;
+                    getCheckAppUserViewModel.ID = values2.UserID;
+                    var token = JWTTokenGenerator.GenerateToken(getCheckAppUserViewModel);
+                    return Ok(token);
+                }
+                else
+                {
+                    return Ok("Başarısız");
+                }
+            }
+        }
+    }
+}
---
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/DTOs/LoginDTOs/GetAppUserIDDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.DTOs.LoginDTOs
+{
+    public class GetAppUserIDDTO
+    {
+        public int UserID { get; set; }
+    }
+}
