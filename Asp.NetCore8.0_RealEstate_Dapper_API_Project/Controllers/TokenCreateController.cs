675bcde5aa738296cfdf3c204b57e0ed4cc02726
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-18 22:53:35 +0300

    JWT Create Token Process Added with API

---
+++ Asp.NetCore8.0_RealEstate_Dapper_API_Project/Controllers/TokenCreateController.cs
@@ -0,0 +1,18 @@
+﻿using Asp.NetCore8._0_RealEstate_Dapper_API_Project.Tools;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenCreateController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateToken(GetCheckAppUserViewModel getCheckAppUserViewModel)
+        {
+            var values = JWTTokenGenerator.GenerateToken(getCheckAppUserViewModel);
+            return Ok(values);
+        }
+    }
+}
