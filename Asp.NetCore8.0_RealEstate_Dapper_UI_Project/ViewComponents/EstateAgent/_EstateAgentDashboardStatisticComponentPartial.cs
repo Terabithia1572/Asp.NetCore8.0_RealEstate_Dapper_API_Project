821bdf25a2733695b47ea1e2eaf5f0a34cefa16c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-09-22 20:14:15 +0300

    Estate Agent Statistic Dashboard Dynamic ID Added

--- Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/EstateAgent/_EstateAgentDashboardStatisticComponentPartial.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/EstateAgent/_EstateAgentDashboardStatisticComponentPartial.cs
@@ -1,17 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.EstateAgent
 {
     public class _EstateAgentDashboardStatisticComponentPartial:ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILoginService _loginService;
 
-        public _EstateAgentDashboardStatisticComponentPartial(IHttpClientFactory httpClientFactory)
+        public _EstateAgentDashboardStatisticComponentPartial(IHttpClientFactory httpClientFactory, ILoginService loginService)
         {
             _httpClientFactory = httpClientFactory;
+            _loginService = loginService;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var id = _loginService.GetUserID;
             #region Statistics1 - ToplamİlanSayısı
             var client1 = _httpClientFactory.CreateClient();
             var responseMessage1 = await client1.GetAsync("https://localhost:44309/api/EstateAgentStatistic/AllProductCount");
@@ -20,19 +24,19 @@ namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.EstateAgen
             #endregion
             #region Statistics2 - EmlakçınınToplamİlanSayısı
             var client2 = _httpClientFactory.CreateClient();
-            var responseMessage2 = await client2.GetAsync("https://localhost:44309/api/EstateAgentStatistic/ProductCountByEmployeeID?id=1");
+            var responseMessage2 = await client2.GetAsync("https://localhost:44309/api/EstateAgentStatistic/ProductCountByEmployeeID?id="+id);
             var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
             ViewBag.employeeByProductCount = jsonData2;
             #endregion
             #region Statistics3 - AktifİlanSayısı
             var client3 = _httpClientFactory.CreateClient();
-            var responseMessage3 = await client3.GetAsync("https://localhost:44309/api/EstateAgentStatistic/StatusProductCountByStatusTrue?id=1");
+            var responseMessage3 = await client3.GetAsync("https://localhost:44309/api/EstateAgentStatistic/StatusProductCountByStatusTrue?id="+id);
             var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
             ViewBag.productCountByEmployeeStatusTrue = jsonData3;
             #endregion
-            #region Statistics4 - Ortalama Kira Sayısı
+            #region Statistics4 - PasifİlanSayısı
             var client4 = _httpClientFactory.CreateClient();
-            var responseMessage4 = await client4.GetAsync("https://localhost:44309/api/EstateAgentStatistic/StatusProductCountByStatusFalse?id=1");
+            var responseMessage4 = await client4.GetAsync("https://localhost:44309/api/EstateAgentStatistic/StatusProductCountByStatusFalse?id="+id);
             var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
             ViewBag.productCountByEmployeeStatusFalse = jsonData4;
             #endregion
