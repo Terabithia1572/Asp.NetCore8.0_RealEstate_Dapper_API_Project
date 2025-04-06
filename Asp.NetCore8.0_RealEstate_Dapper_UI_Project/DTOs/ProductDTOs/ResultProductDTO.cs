d384bc4af208fe303ef8be65c23255cc7e1d4c94
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-06 16:41:36 +0300

    ProductImage DTO Process Added

--- Asp.NetCore8.0_RealEstate_Dapper_UI_Project/DTOs/ProductDTOs/ResultProductDTO.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/DTOs/ProductDTOs/ResultProductDTO.cs
@@ -13,5 +13,6 @@
         public string ProductType { get; set; }
         public string CategoryName { get; set; }
         public bool ProductDailyOfTheDay { get; set; }
+        public DateTime ProductAdvertisementDate { get; set; }
     }
 }
--- Asp.NetCore8.0_RealEstate_Dapper_UI_Project/DTOs/ProductDetailDTOs/GetProductDetailByIDDTO.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/DTOs/ProductDetailDTOs/GetProductDetailByIDDTO.cs
@@ -13,5 +13,6 @@
             public string ProductLocation { get; set; }
             public string ProductVideoURL { get; set; }
             public int ProductID { get; set; }
+        public DateTime ProductAdvertisementDate { get; set; }
     }
 }
