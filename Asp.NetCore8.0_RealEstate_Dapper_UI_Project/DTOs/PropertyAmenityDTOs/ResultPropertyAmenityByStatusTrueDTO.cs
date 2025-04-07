302f26597b597b6ed17d9db51fa6107f9cfa8387
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-07 21:38:29 +0300

    81# Other features of the house Process Added + new ViewComponents Added

---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/DTOs/PropertyAmenityDTOs/ResultPropertyAmenityByStatusTrueDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.PropertyAmenityDTOs
+{
+    public class ResultPropertyAmenityByStatusTrueDTO
+    {
+        public int PropertyAmenityID { get; set; }
+        public string AmenityTitle { get; set; }
+    }
+}
---
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/PropertySingle/_PropertyAmenityStatusTrueByPropertyIDComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.PropertyAmenityDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.PropertySingle
+{
+    public class _PropertyAmenityStatusTrueByPropertyIDComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _PropertyAmenityStatusTrueByPropertyIDComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44309/api/PropertyAmenities?id=1");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPropertyAmenityByStatusTrueDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
