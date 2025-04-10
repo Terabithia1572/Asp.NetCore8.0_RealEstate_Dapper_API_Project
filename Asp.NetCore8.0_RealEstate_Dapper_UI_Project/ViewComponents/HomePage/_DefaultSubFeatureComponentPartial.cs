5c226fbdc81f9bc924e67f5c6a00c327c1a1af8d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-10 22:29:55 +0300

    90# Latest Revisions on Home Page 2 Process Added

--- Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/HomePage/_DefaultSubFeatureComponentPartial.cs
+++ Asp.NetCore8.0_RealEstate_Dapper_UI_Project/ViewComponents/HomePage/_DefaultSubFeatureComponentPartial.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCore8._0_RealEstate_Dapper_UI_Project.DTOs.SubFeatureDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Asp.NetCore8._0_RealEstate_Dapper_UI_Project.ViewComponents.HomePage
 {
     public class _DefaultSubFeatureComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {
-            return View();
+            private readonly IHttpClientFactory _httpClientFactory;
+
+            public _DefaultSubFeatureComponentPartial(IHttpClientFactory httpClientFactory)
+            {
+                _httpClientFactory = httpClientFactory;
+            }
+
+            public async Task<IViewComponentResult> InvokeAsync()
+            {
+                var client = _httpClientFactory.CreateClient();
+                var responseMessage = await client.GetAsync("https://localhost:44309/api/SubFeatures");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<List<ResultSubFeatureDTO>>(jsonData);
+                    return View(values);
+                }
+                return View();
+            }
         }
-    }
 }
